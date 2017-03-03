b057529b08679dff85e08a571fe9be1bf5be1239
Tomasz Rejdych <rejdychtomasz@me.com>
2017-03-03 19:01:04 +0100

    convert project to executable

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "sudoku",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", "0.5.0")
-    ]
+    name: "sudoku"
 )
